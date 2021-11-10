587c71b3f0bb84f6eaca7c56ccd0a3edeb3d1b2b
MarkoKrsic <90420871+MarkoKrsic@users.noreply.github.com>
2021-11-10 17:38:45 -0500

    Added sample scripts for PHP and Godot

---
+++ Godot/HTTP.gd
@@ -0,0 +1,82 @@
+extends Node
+onready var player = get_tree().get_current_scene().find_node("Player")
+onready var level = get_tree().get_current_scene().find_node("Level")
+var contest
+var SecretKey = "***SECRETKEY***"
+var use_ssl=true
+var score
+var playername
+var highgame
+var hashed1
+var hashed2
+var query
+var suburl
+var rankurl
+var rankingurl
+var ranking
+var rank5
+var submitted = false
+
+onready var submit = $SUBMIT
+onready var rank = $GETRANK
+onready var ranks = $GETTOP5
+
+func _ready():
+	score = player.highscore
+	playername = str(player.playerID)
+	#SAMPLE HASH - YOU MUST MATCH THE CONTENTS OF THE PHP FILE FOR THIS TO WORK. 
+	hashedevent = ("EVENTHASH"+SecretKey).md5_text())
+	query = "" #<-----GODOT BUG REQUIRES EMPTY QUERY AND CONTENTS AS PART OF URL . SEE GODOT 3.3 DOCS.
+	suburl = ("https://example.com/foo.php?name="+playername+"&score="+str(score)+"&synca="+str(hashed1))
+	rankurl = ("https://example.com/foo.php?name="+playername+"&score="+str(score)+"&syncb="+str(hashed1))
+	rankingurl = ("https://example.com/foo.php?name="+playername+"&score="+str(score)+"&updatekey="+str(hashed1))
+func _getscore():
+	submit.request(suburl, [] ,use_ssl, HTTPClient.METHOD_POST, query)
+	print(suburl)
+
+func _on_SubmitScore_pressed():
+	if player.playerID == "ANON":
+		level._RankError()
+	else:
+		_getscore()
+
+func _getrank():
+	rank.request(rankurl, [] ,use_ssl, HTTPClient.METHOD_GET, query)
+	print(rankurl)
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+    #SANITY CHECK
+	print (body.get_string_from_utf8())
+	if response_code == 200:
+		submitted == true
+		_getrank()
+	else:
+		level._scoreError()
+		pass
+
+func _on_GETRANK_request_completed(result, response_code, headers, body):
+	 ranking = (body.get_string_from_utf8())
+	 level._drawResults() # <--- make your own function in a seperate script that does what you want to display the result.
+
+func _gettop5():
+	ranks.request(rankingurl, [] ,use_ssl, HTTPClient.METHOD_GET, query)
+
+func _on_GETTOP5_request_completed(result, response_code, headers, body):
+	
+    #SANITY CHECK
+    print(response_code)
+	
+    rank5 = (body.get_string_from_utf8())
+	
+    #SANITY CHECK
+    print(body.get_string_from_utf8())
+	print(rank5)
+	
+    if response_code == 200:
+		level._drawResults() # <--- make your own function in a seperate script that does what you want to display the result.
+
+func _on_Leaderboard_pressed():
+	_gettop5()
+
+
+
---
+++ PHP-REDIS/foo.php
@@ -0,0 +1,62 @@
+<?php
+
+require 'main/lib/Leaderboard.php';
+    
+    $name = $_GET['name'];
+    $score = $_GET['score'];
+    $f1a = $_GET['synca'];
+    $f1b = $_GET['syncb'];
+    $f1c = $_GET['updatekey'];
+    $SecretKey = "***SecretKEY***"; // <----------Pick something complex and random.
+    $lookupHASH = md5($f1c.$SecretKey); //<-----------Sample Hash setup
+    
+//Submit Score
+if ($f1a == $recordHASH){
+    $redis= new Leaderboard('scores');
+    $redis>addMember($name,$score);
+    echo "OK";
+    $redis>close();
+}
+//Get player rank from total players.
+
+if ($f1b == $lookupdHASH){
+    $redis= new Leaderboard('scores');
+    $result = $redis>scoreAndRankFor($name);
+    $total =  $redis>totalMembers();
+
+
+    echo " Rank: ".$result['rank']." of ".$total; 
+
+
+}
+
+
+//TOP 5/10/25 etc , based on page_size in Leaderboard.php. 1 below denotes 1 page of results.
+if ($f1c == $lookupHASH ){
+        $redis= new Leaderboard('scores');
+        $total =  $redis>leaders(1);
+        traverseArray($total);
+        echo "OK";
+        $redis->close();
+
+}
+
+function traverseArray($array)
+{ 
+        // Loops through each element. If element again is array, function is recalled. If not, result is echoed.
+        foreach($array as $key=>$value)
+        {  
+                if(is_array($value))
+                {echo "\n"; 
+                        traverseArray($value); 
+                }else{
+                        echo $key."|".$value."\n"; 
+                } 
+        }
+}
+
+
+
+
+
+?>
